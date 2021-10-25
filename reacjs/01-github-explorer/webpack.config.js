c0bae17c18c17694b44029712c145174be210ad8
Tiago Marques <tiago.marques@softplan.com.br>
2021-10-25 13:55:57 -0300

    [FEAT] Adição da configuração de html-webpack-plugin

--- reacjs/01-github-explorer/webpack.config.js
+++ reacjs/01-github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -7,9 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  resolve: {
+  resolve: { 
     extensions: ['.js', '.jsx']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
